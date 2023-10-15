686d471aef09428b50253bf26454f6e3777d3084
T.Sai Hitesh Reddy <thotlisaihiteshreddy@gmail.com>
2023-10-15 10:43:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import your CSS file
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header>
+        <h1>THOTLI SAI HITESH REDDY</h1>
+        <p>Data Scientist,Web Developer</p>
       </header>
+      <section className="projects">
+        <h2>Projects</h2>
+        <b>Done projects on Sql,Linux</b>
+      </section>
+      <section className="skills">
+        <h2>Skills</h2>
+        <b>Python,C++,C,Java,Sql,Html,CSS,JavaScript,React&NodeJs,Linux,AWS Cloud
+        </b>
+      </section>
+      <section className="experience">
+        <h2>Experience</h2>
+        <b>Worked as Core Member in IEEE</b>
+      </section>
+      <section className="education">
+        <h2>Education</h2>
+        <b>B.Tech with specilization in Data Science</b>
+      </section>
+      <section className="contact">
+        <h2>Contact</h2>
+        <b>thotlisaihiteshreddy@gmail.com</b>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
